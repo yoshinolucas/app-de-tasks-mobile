35aaf68d7c1a1683ae5ff4a19694d551e39264f3
Lucas Yudi Yoshino <lucas.yudi7@gmail.com>
2022-11-29 11:39:15 -0300

    basic widgtes

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,81 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Fundamentals',
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
-      home: Container(),
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                color: Colors.blue,
+                width: 50,
+                height: 50,
+              )
+            ],
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                color: Colors.blue,
+                width: 50,
+                height: 50,
+              )
+            ],
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.purple,
+                  width: 100,
+                  height: 100,
+                )
+              ]),
+          Container(
+            color: Colors.amber,
+            height: 30,
+            width: 300,
+            child: const Text(
+              'Olá Mundo!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black, fontSize: 28
+                ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print('Botão Apertado');
+            },
+            child: const Text(
+              'Aperte o botão!'
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
