7bc273627257cd60ecbaf96c360295ad9edba9fb
Lucas Yudi Yoshino <lucas.yudi7@gmail.com>
2022-11-29 15:03:01 -0300

    aprendendo setstate

--- lib/main.dart
+++ lib/main.dart
@@ -12,61 +12,79 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Fundamentals',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-
-          }
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+        body: ListView(
           children: [
-            Task('Aprender Flutter'),
+            Task('Aprender Flutter aosdjasdjnaofnofsdnojsgdnjsognjfdgndofjgndjgndg'),
             Task('Aprender PHP'),
             Task('Aprender React'),
           ],
         ),
         appBar: AppBar(
-          title: const Text('Task Manager', style: TextStyle(color: Colors.white))
-        ),
+            title: const Text('Task Manager',
+                style: TextStyle(color: Colors.white))),
       ),
     );
   }
 }
 
-
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String titulo;
   const Task(this.titulo, {super.key});
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int _nivel = 0;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(color: Colors.blue, height: 140,),
-          Container(
-            color: Colors.white, height: 100, 
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    
+    return Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Container(
+            child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Column(
               children: [
                 Container(
-                  color: Colors.black26,
-                  width: 72,
-                  height: 100,
-                ),
-                Text(titulo),
-                ElevatedButton(
-                  onPressed: (){},
-                  child: const Icon(Icons.arrow_drop_up),
-                ),
+                    color: Colors.white,
+                    height: 100,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          color: Colors.black26,
+                          width: 72,
+                          height: 100,
+                        ),
+                        Container(
+                          width: 200,
+                          child: Text(
+                            widget.titulo,
+                            style: TextStyle(
+                                fontSize: 24, overflow: TextOverflow.ellipsis),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: ()=>setState(()=>_nivel++),
+                          child: const Icon(Icons.arrow_drop_up),
+                        ),
+                      ],
+                    )),
+                Text('Nível: $_nivel',
+                    style: TextStyle(color: Colors.white, fontSize: 16))
               ],
             )
-          ),
-        ],
-      )
-    );
+          ],
+        )));
   }
 }
